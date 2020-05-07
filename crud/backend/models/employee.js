5b0ddcffd4e49f19ec94887c6b76b527af493052
chirchir <chirchir7370@gmail.com>
2020-05-07 13:16:37 +0300

    initial configurations

---
+++ crud/backend/models/employee.js
@@ -0,0 +1,53 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Employee = sequelize.define('Employee', {
+    id: {
+      allowNull: false,
+      autoIncrement: false,
+      primaryKey: true,
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUID4
+
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        is: ["^[a-z]+$", 'i'],
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        is: ["^[a-z]+$", 'i'],
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      }
+    },
+    dob: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isDate: true,
+      }
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  }, {});
+  Employee.associate = function (models) {
+    // associations can be defined here
+  };
+  return Employee;
+};
