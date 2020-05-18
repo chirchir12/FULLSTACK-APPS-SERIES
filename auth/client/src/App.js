df4c27071830e7e6021ccbabed6a99ff0fe03e0e
chirchir <chirchir7370@gmail.com>
2020-05-18 15:11:41 +0300

    added view profile component

--- auth/client/src/App.js
+++ auth/client/src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './App.css';
-import { Container } from 'react-bootstrap';
 import Register from './components/register/Register';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import Login from './components/Login/Login';
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Container>
+      <div className="container">
         {/* <Login /> */}
-        <Register />
-      </Container>
+        {/* <Register /> */}
+        <Dashboard />
+      </div>
       <Footer />
     </div>
   );
