0a8998ac1a183995336750d2382ffeb114b00b3a
chirchir <chirchir7370@gmail.com>
2020-05-20 13:43:47 +0300

    added white background on login form

--- auth/client/src/components/Login/Login.js
+++ auth/client/src/components/Login/Login.js
@@ -4,7 +4,7 @@ function Login(props) {
   return (
     <div className="row login-row pt-5">
       <div className="col-12 col-md-6 mx-auto">
-        <form className="shadow-lg p-3 rounded">
+        <form className="shadow-lg p-3 rounded bg-light">
           <span className="h3 d-block text-center mb-3">Login</span>
           <div className="form-group">
             <label htmlFor="exampleInputEmail1">Email address</label>
