b7d2e542207eb1e50d813d82b901d0f783e29446
chirchir <chirchir7370@gmail.com>
2020-05-20 13:57:30 +0300

    created fetchprofile function

--- auth/backend/app.js
+++ auth/backend/app.js
@@ -1,21 +1,23 @@
 const express = require('express');
-const ProfileRoute = require('./api/routers/profile')
-const UserRouter = require('./api/routers/user')
+const ProfileRoute = require('./api/routers/profile');
+const UserRouter = require('./api/routers/user');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Handling CORS issues
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
 });
-app.use('/api/auth', UserRouter)
-app.use('/api/user', ProfileRoute)
+app.use('/api/auth', UserRouter);
+app.use('/api/user', ProfileRoute);
 module.exports = app;
-
-
-
-
--- auth/client/src/context/UserContext.js
+++ auth/client/src/context/UserContext.js
@@ -1,6 +1,7 @@
 import React, { useEffect, createContext, useState } from 'react';
-
+const BASE_URL = 'http://localhost:5000/api';
 export const UserContext = createContext();
+
 function UserContextProvider({ props }) {
   const [registerUser, setRegisterUser] = useState({
     firstName: '',
@@ -16,13 +17,24 @@ function UserContextProvider({ props }) {
   const [errors, setErrors] = useState({});
 
   // register
+  const fetchProfile = () => {
+    fetch(`${BASE_URL}/user/profile`)
+      .then((response) => response.json)
+      .then((results) => {
+        setUserProfile(results);
+      })
+      .catch((error) => setErrors(error));
+  };
 
   // login
 
   // updatePassword
 
   // updateProfile
-
+  // side effects
+  useEffect(() => {
+    fetchProfile();
+  }, []);
   return (
     <UserContext.Provider
       value={{
