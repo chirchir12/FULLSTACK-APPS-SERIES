72bde78c14ea293435e886638f40fea6a925afdc
chirchir <chirchir7370@gmail.com>
2020-05-21 18:36:15 +0300

    wrapped login component with container

--- auth/client/src/components/Login/Login.js
+++ auth/client/src/components/Login/Login.js
@@ -21,41 +21,43 @@ function Login(props) {
   }
 
   return (
-    <div className="row login-row pt-5">
-      <div className="col-12 col-md-6 mx-auto">
-        <form
-          onSubmit={handleSubmit}
-          className="shadow-lg p-3 rounded bg-light"
-        >
-          <span className="h3 d-block text-center mb-3">Login</span>
-          <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input
-              type="email"
-              className="form-control"
-              id="exampleInputEmail1"
-              name="email"
-              value={loginUser.email || ''}
-              onChange={handleChange}
-              aria-describedby="emailHelp"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="exampleInputPassword1">Password</label>
-            <input
-              type="password"
-              name="password"
-              value={loginUser.password || ''}
-              onChange={handleChange}
-              className="form-control"
-              id="exampleInputPassword1"
-            />
-          </div>
+    <div className="container">
+      <div className="row login-row pt-5">
+        <div className="col-12 col-md-6 mx-auto">
+          <form
+            onSubmit={handleSubmit}
+            className="shadow-lg p-3 rounded bg-light"
+          >
+            <span className="h3 d-block text-center mb-3">Login</span>
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1">Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                name="email"
+                value={loginUser.email || ''}
+                onChange={handleChange}
+                aria-describedby="emailHelp"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputPassword1">Password</label>
+              <input
+                type="password"
+                name="password"
+                value={loginUser.password || ''}
+                onChange={handleChange}
+                className="form-control"
+                id="exampleInputPassword1"
+              />
+            </div>
 
-          <button type="submit" className="btn btn-style">
-            Submit
-          </button>
-        </form>
+            <button type="submit" className="btn btn-style">
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
