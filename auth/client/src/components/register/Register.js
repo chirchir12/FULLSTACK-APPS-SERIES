ff651fcc0e4af701dd059c4a2959ce886fce2b17
chirchir <chirchir7370@gmail.com>
2020-05-21 18:37:04 +0300

    wrapped register component with container

--- auth/client/src/components/register/Register.js
+++ auth/client/src/components/register/Register.js
@@ -31,85 +31,87 @@ function Register(props) {
   }
 
   return (
-    <div className="row register-row  py-3">
-      <div className="col-12 col-md-7 mx-auto">
-        <form
-          onSubmit={handleSubmit}
-          className="shadow-lg p-3 rounded bg-light"
-        >
-          <span className="h3 d-block text-center mb-3">Register</span>
-          <div className="form-row">
-            <div className="col-12 col-md-6">
-              <div className="form-group">
-                <label htmlFor="firstname">Firstname</label>
-                <input
-                  id="firstname"
-                  name="firstName"
-                  type="text"
-                  value={newUser.firstName || ''}
-                  onChange={handleChange}
-                  className="form-control"
-                  placeholder="First name"
-                  required
-                />
+    <div className="container">
+      <div className="row register-row  py-3">
+        <div className="col-12 col-md-7 mx-auto">
+          <form
+            onSubmit={handleSubmit}
+            className="shadow-lg p-3 rounded bg-light"
+          >
+            <span className="h3 d-block text-center mb-3">Register</span>
+            <div className="form-row">
+              <div className="col-12 col-md-6">
+                <div className="form-group">
+                  <label htmlFor="firstname">Firstname</label>
+                  <input
+                    id="firstname"
+                    name="firstName"
+                    type="text"
+                    value={newUser.firstName || ''}
+                    onChange={handleChange}
+                    className="form-control"
+                    placeholder="First name"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="col-12 col-md-6">
+                <div className="form-group">
+                  <label htmlFor="lastname">Lastname</label>
+                  <input
+                    id="lastname"
+                    name="lastName"
+                    type="text"
+                    value={newUser.lastName || ''}
+                    onChange={handleChange}
+                    className="form-control"
+                    placeholder="Last name"
+                    required
+                  />
+                </div>
               </div>
             </div>
-            <div className="col-12 col-md-6">
-              <div className="form-group">
-                <label htmlFor="lastname">Lastname</label>
-                <input
-                  id="lastname"
-                  name="lastName"
-                  type="text"
-                  value={newUser.lastName || ''}
-                  onChange={handleChange}
-                  className="form-control"
-                  placeholder="Last name"
-                  required
-                />
-              </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1">Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                name="email"
+                value={newUser.email || ''}
+                onChange={handleChange}
+                aria-describedby="emailHelp"
+              />
             </div>
-          </div>
-          <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input
-              type="email"
-              className="form-control"
-              id="exampleInputEmail1"
-              name="email"
-              value={newUser.email || ''}
-              onChange={handleChange}
-              aria-describedby="emailHelp"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="exampleInputPassword1">Password</label>
-            <input
-              type="password"
-              name="password"
-              value={newUser.password || ''}
-              onChange={handleChange}
-              className="form-control"
-              id="exampleInputPassword1"
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="cpassword"> Confirm Password</label>
-            <input
-              type="password"
-              name="cpassword"
-              value={newUser.cpassword || ''}
-              onChange={handleChange}
-              className="form-control"
-              id="cpassword"
-            />
-          </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputPassword1">Password</label>
+              <input
+                type="password"
+                name="password"
+                value={newUser.password || ''}
+                onChange={handleChange}
+                className="form-control"
+                id="exampleInputPassword1"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="cpassword"> Confirm Password</label>
+              <input
+                type="password"
+                name="cpassword"
+                value={newUser.cpassword || ''}
+                onChange={handleChange}
+                className="form-control"
+                id="cpassword"
+              />
+            </div>
 
-          <button type="submit" className="btn btn-style">
-            Register
-          </button>
-        </form>
+            <button type="submit" className="btn btn-style">
+              Register
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
