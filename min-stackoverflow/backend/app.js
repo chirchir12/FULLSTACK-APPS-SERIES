d462aff7fd13b849e9a5149e13a819b01616a7e9
chirchir <chirchir7370@gmail.com>
2020-05-25 10:36:48 +0300

    added user routes in app.js

--- min-stackoverflow/backend/app.js
+++ min-stackoverflow/backend/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const UserRouter = require('./api/routers/user');
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -18,4 +20,6 @@ app.use((req, res, next) => {
 });
 
 // my routes goes here
+app.use('/api/auth', UserRouter);
+
 module.exports = app;
