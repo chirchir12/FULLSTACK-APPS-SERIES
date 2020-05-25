c36ebac1ce6c840d762482954bcf509ed0731d91
chirchir <chirchir7370@gmail.com>
2020-05-25 10:24:07 +0300

    added user routers for signin and register

---
+++ min-stackoverflow/backend/api/routes/users.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { registerUser, loginUser } = require('../controllers/user');
+const router = express.Router();
+
+//1. register user
+router.post('/register', registerUser);
+// 2. login user
+router.post('/login', loginUser);
+
+//3. logout user
+
+module.exports = router;
