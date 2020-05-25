ffd3338a2be438a25c5fb329dc51d371b9118ce4
chirchir <chirchir7370@gmail.com>
2020-05-25 10:01:35 +0300

    updated migration relationship between langcategory and problems

--- min-stackoverflow/backend/migrations/20200525065849-languageCategory-problems.js
+++ min-stackoverflow/backend/migrations/20200525065849-languageCategory-problems.js
@@ -2,22 +2,18 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.addColumn('Problems', 'langId', {
+      type: Sequelize.UUID,
+      references: {
+        model: 'LangCategories',
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+    });
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
-  }
+    return queryInterface.removeColumn('Problems', 'langId');
+  },
 };
