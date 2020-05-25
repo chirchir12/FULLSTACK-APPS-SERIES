2dd0116d81194560c159649f6d34e4e7d6446c65
chirchir <chirchir7370@gmail.com>
2020-05-25 09:48:38 +0300

    updated user-solution migration file to reflect the correct relationships

--- min-stackoverflow/backend/migrations/20200525064659-user-solution.js
+++ min-stackoverflow/backend/migrations/20200525064659-user-solution.js
@@ -2,22 +2,18 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.addColumn('Solutions', 'userId', {
+      type: Sequelize.UUID,
+      references: {
+        model: 'Users',
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+    });
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
-  }
+    return queryInterface.removeColumn('Solutions', 'userId');
+  },
 };
