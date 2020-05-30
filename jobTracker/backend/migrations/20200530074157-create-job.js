596964909639f31517e0d018bda7845f7d4ce68d
chirchir <chirchir7370@gmail.com>
2020-05-30 10:49:30 +0300

    Added model fields

---
+++ jobTracker/backend/migrations/20200530074157-create-job.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Jobs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      company: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      site: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      response: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Jobs');
+  },
+};
