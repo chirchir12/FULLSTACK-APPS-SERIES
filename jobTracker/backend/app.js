d0a5151bd7ea5a6e2caedff69172c536e62e6acb
chirchir <chirchir7370@gmail.com>
2020-05-30 10:37:46 +0300

    Added app.js to manage routes and middlewares

---
+++ jobTracker/backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Handling CORS issues
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
+
+module.exports = app;
