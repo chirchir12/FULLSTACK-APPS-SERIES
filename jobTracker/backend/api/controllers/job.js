ee44a0d8e4ac46beefde690b70b997031113ce9f
chirchir <chirchir7370@gmail.com>
2020-05-30 12:52:01 +0300

    finished creating backend

--- jobTracker/backend/api/controllers/job.js
+++ jobTracker/backend/api/controllers/job.js
@@ -2,11 +2,11 @@ const Job = require('../../models').Job;
 
 //create
 exports.createJob = (req, res) => {
-  const { title, company, site } = req.body;
-  if (!title || !company || !site) {
+  const { title, company, site, link } = req.body;
+  if (!title || !company || !site || !link) {
     return res.status(400).json({ error: 'all fields are required' });
   }
-  Job.create({ title, company, site })
+  Job.create({ title, company, site, link })
     .then((created) => res.status(201).json(created))
     .catch((error) => res.json(400).json({ ...error }));
 };
@@ -22,7 +22,9 @@ exports.updateJob = (req, res) => {
 };
 // all
 exports.getALLJobs = (req, res) => {
-  Job.findAll({ attributes: ['title', 'company', 'site', 'createdAt'] })
+  Job.findAll({
+    attributes: ['id', 'title', 'company', 'site', 'response', 'createdAt'],
+  })
     .then((jobs) => {
       if (jobs.length > 0) {
         return res.status(200).json(jobs);
@@ -32,4 +34,15 @@ exports.getALLJobs = (req, res) => {
     .catch((error) => res.status(404).json(error));
 };
 // delete
-// put
+exports.deleteJob = (req, res) => {
+  Job.destroy({ where: { id: req.body.id } })
+    .then((obj) => res.status(200).json(obj))
+    .catch((error) => res.json(400).json(error));
+};
+// patch
+exports.updateResponse = (req, res) => {
+  const { response } = req.body;
+  Job.update({ response }, { where: { id: +req.params.id } })
+    .then((updated) => res.status(200).json(updated))
+    .catch((error) => res.status(400).json(error));
+};
--- jobTracker/backend/api/routes/job.js
+++ jobTracker/backend/api/routes/job.js
@@ -1,10 +1,19 @@
 const express = require('express');
 
-const { createJob, updateJob, getALLJobs } = require('../controllers/job');
+const {
+  createJob,
+  updateJob,
+  getALLJobs,
+  deleteJob,
+  updateResponse,
+} = require('../controllers/job');
 
 const router = express.Router();
 
 router.post('/create', createJob);
 router.get('', getALLJobs);
 router.put('/update/:id', updateJob);
+router.delete('/delete', deleteJob);
+router.put('/response/:id', updateResponse);
+
 module.exports = router;
