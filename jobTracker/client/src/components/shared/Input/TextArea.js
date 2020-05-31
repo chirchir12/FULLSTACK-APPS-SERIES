c4d07413ba9c3d61bb7d5711aafebf8142d6ba39
chirchir <chirchir7370@gmail.com>
2020-05-31 14:37:51 +0300

    added textarea component

---
+++ jobTracker/client/src/components/shared/Input/TextArea.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function TextArea(props) {
+  return (
+    <div className="form-group">
+      <label htmlFor={props.id}>{props.label}</label>
+      <textarea
+        value={props.value}
+        class="form-control"
+        id={props.id}
+        rows={props.rows}
+      ></textarea>
+    </div>
+  );
+}
+
+export default TextArea;
