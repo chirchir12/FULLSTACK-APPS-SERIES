dc198c128c65e38854ba3b71ab371841be85a616
chirchir <chirchir7370@gmail.com>
2020-05-31 14:33:45 +0300

    added trucate package to limit text in card

--- jobTracker/client/src/components/Home/Card/Card.js
+++ jobTracker/client/src/components/Home/Card/Card.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import moment from 'moment';
+import truncate from 'truncate';
 import { Link } from 'react-router-dom';
 
 function Card(props) {
@@ -11,7 +12,7 @@ function Card(props) {
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <h6 className="card-subtitle mb-2 text-muted">{company}</h6>
-          <p className="card-text">{responsibility}</p>
+          <p className="card-text">{truncate(responsibility, 220)}</p>
           <a href="#" className="btn btn-sm btn-outline-primary card-link">
             Visit Site
           </a>
